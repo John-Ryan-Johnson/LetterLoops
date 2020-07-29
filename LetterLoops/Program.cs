d9d21181572b429ea2448e9adbdbdc3fb24af586
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-07-28 20:20:42 -0500

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LetterLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some random letters.");
+            string randomLetters = Console.ReadLine();
+
+            string result = string.Empty;
+
+            for (var i = 0; i < randomLetters.Length; i++)
+            {
+                char c = randomLetters[i];
+
+                result += char.ToUpper(c);
+
+                result += new string(char.ToLower(c), i);
+
+                if (i < randomLetters.Length - 1)
+                {
+                    result += "-";
+                }
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
